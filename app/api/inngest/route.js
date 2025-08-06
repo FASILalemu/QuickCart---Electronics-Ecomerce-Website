07abc573d3b6f47f8c58592368a01ae0662db259
FASILalemu <fasilalemu55@gmail.com>
2025-08-06 11:03:24 +0300

    fix error

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,11 @@
 import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
-import { helloWorld } from "../../../inngest/functions";
+// import { helloWorld } from "../../../inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    helloWorld, // <-- This is where you'll always add all your functions
+    // helloWorld, // <-- This is where you'll always add all your functions
 
     syncUserCreation,
     syncUserUpdation,
