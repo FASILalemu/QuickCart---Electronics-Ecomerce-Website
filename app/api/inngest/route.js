5a2f1052f6229709da44a1a7844029200eef9b34
FASILalemu <fasilalemu55@gmail.com>
2025-08-07 09:40:11 +0300

    error fix

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,14 +1,9 @@
+// app/api/inngest/route.js
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
-// import { helloWorld } from "../../../inngest/functions";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest.js";
 
-export const { GET, POST, PUT } = serve({
+// Only export POST handler for Inngest webhooks
+export const { POST } = serve({
   client: inngest,
-  functions: [
-    // helloWorld, // <-- This is where you'll always add all your functions
-
-    syncUserCreation,
-    syncUserUpdation,
-    syncUserDeletion
-  ],
+  functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
 });
--- config/inngest.js
+++ config/inngest.js
@@ -26,7 +26,7 @@ export const syncUserCreation = inngest.createFunction(
         }
 
         await connectDb()
-        await User.create(userData)
+        await user.create(userData)
     }
 )
 
